196b1de35195640db9f7ecf9ab013b97f4fb85f4
LucasYuNju <LucasYuNju@gmail.com>
2016-09-22 13:50:23 +0800

    Add electron

---
+++ index.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1000, height: 700});
+  mainWindow.loadURL(`file://${__dirname}/public/index.html`);
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+})
